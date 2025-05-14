367e69dbaa5f7531a9d1028872fdd5785c7c1b54
Tarcisio Bispo <tbisp0@hotmail.com>
2025-05-14 19:45:02 -0300

    Update App

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter>      
     </TooltipProvider>
   </QueryClientProvider>
 );

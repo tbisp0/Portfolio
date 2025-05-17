242bf157c42c117acabe07f3b4b20fc76ec65d4b
Tarcisio Bispo <tbisp0@hotmail.com>
2025-05-17 11:43:24 -0300

    P por p

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
